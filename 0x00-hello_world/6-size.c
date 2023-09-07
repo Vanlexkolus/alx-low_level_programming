4e71a54689622e5fa37599adcc27bfdcbdd51446
root <kolurejoalexanderola@gmail.com>
2023-09-07 16:48:38 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,12 @@
  * Return: this returns 0 if there's no error in my code
  */
 
-int main(void)
+int main7(void)
 {
-    fprintf(stdout, "Size of a char: %lu byte(s)\n", sizeof(char));
-    fprintf(stdout, "Size of an int: %lu byte(s)\n", sizeof(int));
-    fprintf(stdout, "Size of a long int: %lu byte(s)\n", sizeof(long int));
-    fprintf(stdout, "Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-    fprintf(stdout, "Size of a float: %lu byte(s)\n", sizeof(float));
-
-    return (0);
+	fprintf(stdout, "Size of a char: %lu byte(s)\n", sizeof(char));
+	fprintf(stdout, "Size of an int: %lu byte(s)\n", sizeof(int));
+	fprintf(stdout, "Size of a long int: %lu byte(s)\n", sizeof(long int));
+	fprintf(stdout, "Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	fprintf(stdout, "Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
