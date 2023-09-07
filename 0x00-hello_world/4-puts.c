67a7c379d85f4570c0b351c17235e3a4644e6662
root <kolurejoalexanderola@gmail.com>
2023-09-07 10:20:24 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - This is a main file
+ * Return: returns 0 if successful
+ */
 
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a\
+	multilingual puzzle");
 	return (0);
 }
