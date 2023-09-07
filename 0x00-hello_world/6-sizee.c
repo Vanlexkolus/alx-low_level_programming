8f560e47494fc7bb95d3512bdebe3be524ebf606
root <kolurejoalexanderola@gmail.com>
2023-09-07 16:38:20 -0700

    task 6

---
+++ 0x00-hello_world/6-sizee.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - The main function is void
+ *
+ * Return: this returns 0 if there's no error in my code
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
