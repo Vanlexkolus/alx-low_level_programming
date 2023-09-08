92b6154f1328edd009ac7f9eea4c61d52daadb68
root <kolurejoalexanderola@gmail.com>
2023-09-07 19:02:55 -0700

    Last Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - This program prints the below text
+ * Return: This program successfully returns 1
+ */
 int main(void)
 {
-	fprintf(stdout"and that piece of art is useful" \- \Dora Korpar, 2015-10-19 \n");
+	fprintf(stdout, "and that piece of art is useful\"-"
+	"Dora Korpar, 2015-10-19\n");
 	return (1);
 }
