3b882fa63db014a12055a4638d3d53277af81125
root <kolurejoalexanderola@gmail.com>
2023-09-09 10:49:08 -0700

    It works

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, u);
 	}
-	else(u == 0)
+	else(u == 0);
 	{
 		printf("Last digit of %d is %d and is 0\n", n, u);
 	}
