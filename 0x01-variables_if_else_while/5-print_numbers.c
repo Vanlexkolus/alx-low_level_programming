937e5cc6108e7248a8810b7266140192c544f0c3
root <kolurejoalexanderola@gmail.com>
2023-09-09 20:23:31 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is void
+ *
+ * Return: always return zero for this code
+ */
+
+int main(void)
+{
+	char X = '0';
+
+	while (X <= '9')
+	{
+		putchar(X);
+		X++;
+	}
+	putchar('\n');
+	return (0);
+}
