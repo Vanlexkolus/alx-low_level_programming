2157c9367e819507bb4b9faf001e1fa9a3f57fcf
root <kolurejoalexanderola@gmail.com>
2023-09-13 07:01:39 -0700

    added comments to 1-alph

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,14 @@
  */
 
 void print_alphabet(void)
-
 {
 	char A = 'a';
-	
-        while (A <= 'z')
-	{
-		_putchar(A);
-		A++;
+	{	
+		while (A <= 'z')
+		{
+			_putchar(A);
+			A++;
+		}
 	}
 	_putchar('\n');
-
 }
