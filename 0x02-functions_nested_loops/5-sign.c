35bc77dc44b09a742106c3923a767a0755a1f4d6
root <kolurejoalexanderola@gmail.com>
2023-09-13 09:26:59 -0700

    add task five to the repo

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - This function prints either +, -, or 0
+ * @n: n returns either 1, -1, or 0
+ * Return: returns the following
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	if (n == 0)
+	{
+		_putchar (0);
+		return (0);
+	}
+	return (0);
+}
