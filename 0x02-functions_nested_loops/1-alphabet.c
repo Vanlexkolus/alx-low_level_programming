97bb69905f2f2189ffa57870d9965a4687313cfc
Vanlex_Kolus <kolurejoalexanderola@gmail.com>
2023-09-13 02:30:33 +0100

    print abc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<main.h>
+/**
+ * main - this code prints alphabets in small letters
+ *
+ * Return: always return zero
+ */
+
+int main(void)
+{
+	char u = 'a';
+	char v = 'z';
+		if (u <= z)
+		{
+			putchar ('u');
+			u++;
+		}
+
+	return (0);
+}
