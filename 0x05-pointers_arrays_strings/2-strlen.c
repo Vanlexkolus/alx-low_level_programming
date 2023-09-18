73cdc154fc705ff96faf480ab6c3461af9e59707
root <kolurejoalexanderola@gmail.com>
2023-09-18 16:30:08 -0700

    cont on mob

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,20 @@
+#include "main.h"
+
+/**
+ *  _strlen - a function to list string length
+ *
+ * @s: character
+ * Retun: lnt
+ */
+
 int _strlen(char *s)
+{
+	int lnt;
+
+	while (lnt != '\0')
+	{
+		lnt++;
+		s++;
+	}
+	return (lnt);
+}
