c0445aec5c6ee4531a20026be6b0ae0b35430e36
Vanlex_Kolus <kolurejoalexanderola@gmail.com>
2023-09-23 03:31:52 +0100

    betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	j = 0;
 	while (j < n && src[j] != '\0')
-        {
+	{
 		dest[j] = src[j];
 		j++;
 	}
-	
+
 	while (j < n)
-        {
+	{
 		dest[j] = '\0';
 		j++;
 	}
