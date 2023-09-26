559bdfaf2cdb2fdd74e2dd96deee4f162c268e9f
root <kolurejoalexanderola@gmail.com>
2023-09-26 15:41:27 -0700

    done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
+/**
+ * _strspn - compares the input and print byte size
+ * @s: char one
+ * @accept: char two
+ * Return: int
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	int a;
-	int b = 0;
+	unsigned int b = 0;
 
-	for (a = 0; s[a] != '\0'; a++)
+	while (*s)
 	{
-		if (s[a] == *accept)
+		for (a = 0; accept[a]; a++)
 		{
-			accept[b] = a;
-			b++;
+			if (*s == accept[a])
+			{
+				b++;
+				break;
+			}
+			else if (accept[a + 1] == '\0')
+			{
+				return (b);
+			}
 		}
+		s++;
 	}
 	return (b);
 }
