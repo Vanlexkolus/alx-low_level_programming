87eecb9cd46ac747e38565b8e6b032dbcf542f58
root <kolurejoalexanderola@gmail.com>
2023-10-02 10:26:41 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * If you rename the program, it will print the new name, without having to compile it again
+ * You should not remove the path before the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: zero
+ */
+
+int main(int argc, char* argv[])
+{
+	printf ("%s\n", argv[argc - 1]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+How to use arguments passed to my  program
+the two prototypes of main that I know of, and in which case I use one or the other
+How to use __attribute__((unused)) or (void) to compile functions with unused variables or parameters
