f4f7a96a164555c8f4d10715179ebaad9054c583
root <kolurejoalexanderola@gmail.com>
2023-10-03 11:41:52 -0700

    done with static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#Create a script called create_static_lib.sh that creates a static library called
-#liball.a from all the .c files that are in the current directory.
-gcc -c *.o
-ar rc liball.a *.o
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+ar -rc liball.a *.o
 ranlib liball.a
