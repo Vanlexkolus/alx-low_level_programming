1d1cc752900bb1f27ae9d29ec4cc29459fd837ec
root <kolurejoalexanderola@gmail.com>
2023-10-10 18:49:23 -0700

    just like butter :)

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main function to start the program
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
