99aed406317e6c1e4d1d8ccee7eb4fb7ebbde76b
root <kolurejoalexanderola@gmail.com>
2023-10-11 13:25:39 -0700

    dobe

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,13 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+ * print_name - name of function
+ * @name: the name
+ * @f: pointer of the funtion
+ * Return: VOid
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f != NULL)
