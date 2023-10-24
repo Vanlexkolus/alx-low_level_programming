757006272673644d0187187ac7c91ee614b37c97
root <kolurejoalexanderola@gmail.com>
2023-10-24 05:54:25 -0700

    wrote a function that return the number of elements in a node

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * listint_len - a function that return's the total num of elements in  node
+ * @h: the 'head' pointer
+ * Return: Returns the number of elements in a node
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *current = h;
