bce017b3849ea2577767c3d07ad6723f0312bc3f
root <kolurejoalexanderola@gmail.com>
2023-10-24 18:14:51 -0700

    a function that frees the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a list
+ * @head: head pointer to address
+ * Returns: returns void (nothing)
+ */
+
+void free_listint2(listint_t **head)
+{
+        listint_t *temporaryNode;
+
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			temporaryNode = *head;
+			*head = temporaryNode->next;
+			free(temporaryNode);
+		}
+	}
+}
