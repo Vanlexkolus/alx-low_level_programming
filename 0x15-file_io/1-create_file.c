4e7e0e72bb8985fac9294265d823191dd01b09c9
root <kolurejoalexanderola@gmail.com>
2023-10-30 20:01:14 -0700

    wrote a function that creates a new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a new file.
+ * @filename: pointer to filename
+ * @text_content: string to be written to file
+ * Return: success(1) or failure(-1)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int doc, u, wr;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	doc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	
+	if (doc == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		u = 0;
+		while (text_content[u])
+			u++;
+		wr = write(doc, text_content, u);
+		if (wr != u)
+			return (-1);
+	}
+	close(doc);
+	return (1);
+}
