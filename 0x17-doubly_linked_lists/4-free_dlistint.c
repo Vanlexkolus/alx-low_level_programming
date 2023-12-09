5bb09de74d83140907faac4ece8ad772c3c9aaf9
root <kolurejoalexanderola@gmail.com>
2023-12-08 16:56:05 -0800

    done with this

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  * free_dlistint - frees the lists
+  * @head: head of list
+  * Return: void
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
