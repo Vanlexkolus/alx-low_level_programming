b0f29a6e8277d5eae1026b02ec41483cca0a760c
Alexander_Olamide_Kolurejo <kolurejoalexanderola@gmail.com>
2023-12-12 02:25:28 +0100

    removed the directory i created

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - this function cal the absolute value of an integer
+ * @g: g collect the data
+ * Return: return zero
+ */
+
+int _abs(int g)
+{
+	if (g >= 0)
+	{
+		return (g);
+	}
+	else
+	{
+		return (-g);
+	}
+	return (0);
+}
