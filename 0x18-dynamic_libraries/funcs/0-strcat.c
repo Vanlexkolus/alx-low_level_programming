21d889b0d6b555bbbaaa6e847eb4a679d4094f6e
Alexander_Olamide_Kolurejo <kolurejoalexanderola@gmail.com>
2023-12-12 02:11:54 +0100

    This is fairly Easy -fPIC

---
+++ 0x18-dynamic_libraries/funcs/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - add two strings together
+ * @dest: first char
+ * @src: second character
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
