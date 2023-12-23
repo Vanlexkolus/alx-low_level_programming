8da3c520bf47f7d9d76515feee3e8d52b844a322
root <kolurejoalexanderola@gmail.com>
2023-12-22 20:42:07 -0800

    wrote all the necessary files

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_get - Gets an item from the hash function
+  * @ht: hash table to work with
+  * @key: key to find
+  * Return: the value of the associated key
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
